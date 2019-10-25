27967cf10d0cc108167683ae89c63ffbd1e15aa1
PonzyPon <11908295+PonzyPon@users.noreply.github.com>
2019-10-25 13:29:23 +0900

    Save velocity

--- popup.js
+++ popup.js
@@ -4,6 +4,10 @@ velocity = document.getElementById("velocity");
 
 // event bind
 calculateButton.onclick = function (element) {
+    chrome.storage.sync.set({ velocity: velocity.value }, function () {
+        console.log("velocity " + velocity.value + ' was saved.');
+    });
+
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {
             type: "SHOW_PROGRESS_PREDICTION",
@@ -11,3 +15,10 @@ calculateButton.onclick = function (element) {
         });
     });
 };
+
+chrome.storage.sync.get('velocity', function (data) {
+    if (data.velocity === void 0) {
+        return;
+    }
+    velocity.value = data.velocity;
+});
